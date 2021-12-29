33668c63ddc1ee899f7a4719a9da6b90733670ec
Vrisel <vrisel.p@gmail.com>
2021-12-29 11:26:25 +0900

    FEATURE: 기본 세팅

---
+++ components/layout.js
@@ -0,0 +1,79 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+
+const name = "브리셀";
+export const siteTitle = "절대음감 테스트";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="절대음감에 도전해보자!" />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={`${name}의 프로필 이미지`}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+      <footer className={styles.footer}>
+        <a href="https://vrisel.github.io/" target="_blank" rel="noreferrer">
+          Made by
+          <strong className={styles.logo}>
+            VRISEL{" "}
+            <Image src="/vrisel.png" alt="Vrisel Logo" width={16} height={16} />
+          </strong>
+        </a>
+      </footer>
+    </div>
+  );
+}
