46b262b7e627eb1b28f6c78dcbf1d2a23d8ea582
Vrisel <vrisel.p@gmail.com>
2022-02-04 11:26:32 +0900

    FEATURE: userAgent 띄워주는 페이지

---
+++ pages/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ agent }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheTest',
+  data() {
+    return { agent: '' };
+  },
+  mounted() {
+    this.agent = navigator.userAgent;
+  },
+};
+</script>
