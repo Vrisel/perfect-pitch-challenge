2b39f557c6e5d56be9d1caf85379a037fd44421e
Vrisel <vrisel.p@gmail.com>
2022-02-04 15:31:25 +0900

    99f046a

---
+++ _nuxt/static/1643956268/challenge/hard/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/challenge/hard", {data:[{pitch:440}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1643956268/challenge/survival/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/challenge/survival", {data:[{pitch:440}],fetch:{},mutations:void 0});
